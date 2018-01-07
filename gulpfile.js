c8c07a4d94bf2fba64202fc91010e78e02ff40d1
Natalia Norboeva <natanor@softomate.com>
2018-01-07 21:08:10 +0700

    pug is added

--- gulpfile.js
+++ gulpfile.js
@@ -1,85 +1,77 @@
-var gulp = require('gulp');  
-var sass = require('gulp-sass');
-var uglify = require('gulp-uglify');
-var rename = require('gulp-rename');
-var notify = require('gulp-notify');
-var minifycss = require('gulp-minify-css');
-var concat = require('gulp-concat');
-var plumber = require('gulp-plumber');
-var browserSync = require('browser-sync').create();
+const gulp = require('gulp');
+const plugins = require('gulp-load-plugins')();
+const browserSync = require('browser-sync').create();
 
-gulp.task('default', ['dev']);
+gulp.task('default', plugins.sequence('clean', 'dev'));
 
-gulp.task('dev', ['build-dev', 'browser-sync', 'watch']);
+gulp.task('production', plugins.sequence('clean', 'build'));
 
-gulp.task('build-dev', ['html', 'assets', 'sass', 'scripts']);
+gulp.task('dev', plugins.sequence('build', 'serve', 'watch'));
 
-/* Scripts task */
-gulp.task('html', function () {
-    return gulp.src('src/**/*.html')
-        .pipe(gulp.dest('docs/'));
+gulp.task('build', ['pug', 'assets', 'styles', 'scripts']);
+
+gulp.task('pug', function() {
+  return gulp.src(['src/*.pug','!src/_*.pug'])
+    .pipe(plugins.pug({
+      pretty: '\t'
+    }))
+    .pipe(gulp.dest('build/'));
 });
 
 gulp.task('assets', function() {
-    return gulp.src('src/assets/**/*.*')
-        .pipe(gulp.dest('./docs/'));
+  return gulp.src('src/assets/**/*.*')
+    .pipe(gulp.dest('./build/'));
 });
 
-gulp.task('sass', function() {
-    return gulp.src('src/styles/global.scss') 
-        .pipe(plumber({ // plumber - плагин для отловли ошибок.
-            errorHandler: notify.onError(function(err) { // nofity - представление ошибок в удобном для вас виде.
-                return {
-                    title: 'Styles',
-                    message: err.message
-                }
-            })
-        }))
-        .pipe(sass())
-        .pipe(concat('styles.css'))
-        .pipe(minifycss())
-        .pipe(rename({suffix: '.min'}))
-        .pipe(gulp.dest('docs/css'));
-        //.pipe(reload({stream:true}));
+gulp.task('styles', function() {
+  return gulp.src('src/styles/global.scss')
+    .pipe(plugins.plumber({ // plumber - плагин для отловли ошибок.
+      errorHandler: plugins.notify.onError(function(err) { // nofity - представление ошибок в удобном для вас виде.
+        return {
+          title: 'Styles',
+          message: err.message
+        }
+      })
+    }))
+    .pipe(plugins.sass())
+    .pipe(plugins.autoprefixer({
+      browsers: ['last 2 versions']
+    }))
+    .pipe(plugins.concat('styles.css'))
+    .pipe(plugins.cleanCss())
+    .pipe(plugins.rename({suffix: '.min'}))
+    .pipe(gulp.dest('build/css'));
 });
 
 gulp.task('scripts', function() {
   return gulp.src(
     /* Add your JS files here, they will be combined in this order */
-    'src/scripts/modernizr.js'
-    )
-    //.pipe(concat('main.js'))
-    //.pipe(gulp.dest('js')) /*нужна ли эта строчка?*/
-    //.pipe(rename({suffix: '.min'}))
-    //.pipe(uglify())
-    .pipe(gulp.dest('docs/js'));
+    'src/scripts/*.js'
+  )
+    //.pipe(plugins.uglify())
+    .pipe(gulp.dest('build/js'));
+});
+
+gulp.task('clean', function() {
+  return gulp.src('build/')
+    .pipe(plugins.clean());
 });
 
 /* Prepare Browser-sync for localhost */
-gulp.task('browser-sync', function() {
-    browserSync.init(['css/*.css', 'js/*.js'], {
-        /*
-        I like to use a vhost, WAMP guide: https://www.kristengrote.com/blog/articles/how-to-set-up-virtual-hosts-using-wamp, XAMP guide: http://sawmac.com/xampp/virtualhosts/
-        
-        proxy: 'my_dev_site.url',*/
+gulp.task('serve', function() {
+  browserSync.init(['css/*.css', 'js/*.js'], {
+      /* For a static server you would use this: */
+    server: {
+      baseDir: 'build/'
+    }
 
-        /* For a static server you would use this: */        
-        server: {
-            baseDir: 'docs/'
-        }
-        
-    });
-});
-
-/* Reload task */
-gulp.task('bs-reload', function () {
-    browserSync.reload();
+  });
 });
 
 /* Watch scss, js and html files, doing different things with each. */
 gulp.task('watch', function () {
-    gulp.watch('src/styles/**/*.scss', ['sass'])
-    gulp.watch('src/scripts/**/*.js', ['scripts'])
-    gulp.watch('src/**/*.html', ['html'])
-    gulp.watch('src/**/*.*', ['bs-reload']);
+  gulp.watch('src/styles/**/*.scss', ['styles']);
+  gulp.watch('src/scripts/**/*.js', ['scripts']);
+  gulp.watch('src/assets/**/*.*', ['assets']);
+  gulp.watch('src/**/*.*').on('change', browserSync.reload); //Перезапуск browserSynс
 });
